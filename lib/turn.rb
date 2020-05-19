6d916363ceb801f3c94023d1e826cee9c40a5e29
Ermite Rikudo <opouth@hotmail.fr>
2020-05-19 01:13:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,14 @@
+ttt_board = ["","X"," "," "," "," "," "," "," "]
+#asks for input
+puts "Enter a position on the board 1-9:"
+#get input
+user_input = gets.chomp
+#convert input to index
+number_entered = user_input.to_i - 1
+
+
 # LIB/turn.rb files begin here:
 #_________________________________________________
-
-def input_to_index(input)
-  input.to_i - 1
-end
 
 #if index is valid...
 def valid_move?(number_entered, board)
@@ -14,8 +19,8 @@ def move(array, index, name = "X")
   array[index] = name
 end
 #if index is valid...show the board
-def position_taken?(board, answer)
-  board[answer] != " "
+def position_taken?(board, index)
+ !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
 def display_board(board)
@@ -26,46 +31,20 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#this method need to be developed ________________
-def turn(board)
-    puts "Please enter 1-9:"
-    answer = gets.chomp
-    answer = input_to_index(answer)
-    if valid_move?(answer, board)
-      move(board, answer)
-      puts display_board(board)
-    else
-      puts "That is an invalid entry!"
-      turn(board)
-    end
-end
+final_position = number_entered()
 
 #else ask for input again until you get valid input
 #_________________________________________________
 
-board = ["X"," "," "," "," "," "," "," "," "]
-puts "Welcome to Tic Tac Toe!"
-puts display_board(board)
-turn(board)
+#if index is valid
+if valid_move?(number_entered, board)
+#   - make the move for index
+  move(ttt_board, number_entered)
+#   - show the board
+  display_board(ttt_board)
+#else ask tor input again until you get a valid input
 
 
-=begin
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[0]} | #{board[0]} | #{board[0]} "
-end
-display_board(board)
-
-puts "Choose your Move case between 1-9"
-choice = gets.strip.to_i
-taken = choice
-
-def input_to_index(choice)
-new_index = choice - 1
-end
-input_to_index(choice)
-=end
+#?final_position = input_to_index(input_position)
+#?move(board, final_position)
+#?display_board(board)
