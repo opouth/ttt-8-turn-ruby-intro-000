13eda7249aec4cb055cfed789380afdf1eb60e79
Ermite Rikudo <opouth@hotmail.fr>
2020-05-19 01:25:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   #input to index
   index = input_to_index(user_input)
   token = current_player(board)
-  
+
   #check for validation
   if valid_move?(board,index)
     puts 'valid move'
@@ -90,7 +90,7 @@ end
 def move(board, index, token)
   board[index] = token
   play(board)
-  
+
 end
 
 
@@ -119,7 +119,7 @@ def check_win_combination?(board, player, win_combo)
   end
 end
 
-#IS BOARD FULL? 
+#IS BOARD FULL?
 def full?(board)
   if board.include?(' ') || board.include?('')
     return false
